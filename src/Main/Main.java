746baca7d21c8ea684f07ff9d83509f0328d434c
marieudam <marieudam@gmail.com>
2016-10-01 14:02:21 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
-		System.out.println("Google'i otsitulemuse positsioon");
+		System.out.println("Google'i otsitulemuse positsiooni leidmine");
 		System.out.println("Autor: Marie");
 	    	System.out.println("Aasta: 2016");
     }
