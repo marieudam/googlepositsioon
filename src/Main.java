9d8461d3e18f53cf9fc1d86cd4f0ce6eabae9da5
Marie Udam <marieudam@gmail.com>
2016-10-16 19:14:10 +0300

    muudatus nimes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Google'i otsitulemuse positsiooni leidmine");
+        System.out.println("Autor: Marie Udam");
+        System.out.println("Aasta: 2016");
+    }
+}
--- src/Main/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-		System.out.println("Google'i otsitulemuse positsioon");
-		System.out.println("Autor: Marie");
-	    	System.out.println("Aasta: 2016");
-    }
-}
