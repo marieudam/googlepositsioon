265d16803adda5aaa3733b9be365b5adaa638b52
Marie Udam <marieudam@gmail.com>
2016-10-17 19:52:35 +0300

    sisendi küsimine

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
 		System.out.println("Google'i otsitulemuse positsiooni leidmine");
 		System.out.println("Autor: Marie");
-	    	System.out.println("Aasta: 2016");
+		System.out.println("Aasta: 2016");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Sisesta märksõna, mida otsid:");
+		String input = sc.nextLine();
     }
 }
