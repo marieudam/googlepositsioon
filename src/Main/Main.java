c45bd272b846c1b322029d4417b8892d312fe66a
Marie Udam <marieudam@gmail.com>
2016-10-22 19:54:26 +0300

    lihtne UI

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,44 @@
-import java.util.Scanner;
+package Main;
 
-public class Main {
-    public static void main(String args[]) {
-		System.out.println("Google'i otsitulemuse positsiooni leidmine");
-		System.out.println("Autor: Marie");
-		System.out.println("Aasta: 2016");
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-		Scanner sc = new Scanner(System.in);
+public class Main extends Application{
+    Button nupp;
+    @Override
+	public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Märksõna positsiooni leidja");
+        nupp = new Button();
+        nupp.setText("Kliki siin");
 
-		System.out.println("Sisesta märksõna, mida otsid:");
-		String input1 = sc.nextLine();
-		System.out.println("Sisesta piirkond, kust otsid:");
-		String input2 = sc.nextLine();
-		// Programm guugeldab sisestatud parameetritele vastavaid tulemusi
-		System.out.println("Sisesta domeen:");
-		String input3 = sc.nextLine();
-		// Programm otsib Google'i tulemustest sobiva tulemuse välja ning kuvab selle positsiooni
+        StackPane kujundus = new StackPane();
+        kujundus.getChildren().add(nupp);
 
+        Scene stseen = new Scene(kujundus, 400, 400);
+        primaryStage.setScene(stseen);
+        primaryStage.show();
     }
 }
+
+//public class Main{
+//    public static void main(String args[]) {
+//        System.out.println("Google'i otsitulemuse positsiooni leidmine");
+//        System.out.println("Autor: Marie");
+//        System.out.println("Aasta: 2016");
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Sisesta märksõna, mida otsid:");
+//        String input1 = sc.nextLine();
+//        System.out.println("Sisesta piirkond, kust otsid:");
+//        String input2 = sc.nextLine();
+//        // Programm guugeldab sisestatud parameetritele vastavaid tulemusi
+//        System.out.println("Sisesta domeen:");
+//        String input3 = sc.nextLine();
+//        // Programm otsib Google'i tulemustest sobiva tulemuse välja ning kuvab selle positsiooni
+//
+//    }
