91286987c3bdc68d40f535941512510d3a49f180
Marie Udam <marieudam@gmail.com>
2016-10-23 14:49:05 +0300

    UI update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,26 +1,67 @@
 package Main;
 
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-    Button nupp;
+    Stage aken;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
 	public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Märksõna positsiooni leidja");
-        nupp = new Button();
-        nupp.setText("Kliki siin");
+        aken = primaryStage;
+        aken.setTitle("Märksõna positsiooni leidja");
 
-        StackPane kujundus = new StackPane();
-        kujundus.getChildren().add(nupp);
+        GridPane raamistik = new GridPane();
+        raamistik.setPadding(new Insets(10, 10, 10, 10));
+        raamistik.setVgap(10);
+        raamistik.setHgap(10);
 
-        Scene stseen = new Scene(kujundus, 400, 400);
-        primaryStage.setScene(stseen);
-        primaryStage.show();
+        //Märksõna tekst
+        Label tekst1 = new Label("Sisesta märksõna:");
+        GridPane.setConstraints(tekst1, 0, 0);
+
+        //Märksõna lahter
+        TextField lahter1 = new TextField();
+        lahter1.setPromptText("Märksõna");
+        GridPane.setConstraints(lahter1, 1, 0);
+
+        //Domeeninime tekst
+        Label tekst2 = new Label("Sisesta domeeninimi:");
+        GridPane.setConstraints(tekst2, 0, 1);
+
+        //Domeeninime lahter
+        TextField lahter2 = new TextField();
+        lahter2.setPromptText("Domeeninimi");
+        GridPane.setConstraints(lahter2, 1, 1);
+
+        //Asukoha tekst
+        Label tekst3 = new Label("Sisesta asukoht:");
+        GridPane.setConstraints(tekst3, 0, 2);
+
+        //Asukoha lahter
+        TextField lahter3 = new TextField();
+        lahter3.setPromptText("Asukoht");
+        GridPane.setConstraints(lahter3, 1, 2);
+
+        //Otsingu nupp
+        Button nupp = new Button("Otsi");
+        GridPane.setConstraints(nupp, 1, 3);
+
+        raamistik.getChildren().addAll(tekst1, lahter1, tekst2, lahter2, tekst3, lahter3, nupp);
+
+        Scene stseen = new Scene(raamistik, 400, 300);
+        aken.setScene(stseen);
+        aken.show();
     }
 }
 
